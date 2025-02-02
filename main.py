0e29396fe3bf5817543c3eade8a6597b8e93c7f5
EvanRunner <evanrunnercontact@gmail.com>
2025-02-02 00:37:15 -0600

    emtire thing

---
+++ main.py
@@ -0,0 +1,7 @@
+import webview
+
+# Create a window with the specified URL
+webview.create_window("Counter-Strike", "file:///c%3A/Users/evanr/OneDrive/Documents/counter-strike%20web/index.html")
+
+# Start the webview window
+webview.start()
